a6cb4904ef342a3de34584a28a7758b47dfc31c0
bee-joo <bukin-danila@yandex.ru>
2022-09-19 20:50:33 +0300

    Add menu map

---
+++ src/main/java/com/example/birdwikibot/telegram/command/Menu.java
@@ -0,0 +1,32 @@
+package com.example.birdwikibot.telegram.command;
+
+import com.example.birdwikibot.telegram.command.service.BackCommand;
+import com.example.birdwikibot.telegram.command.service.RandomCommand;
+import com.example.birdwikibot.telegram.command.subscription.ChangeTimeCommand;
+import com.example.birdwikibot.telegram.command.subscription.SubscribeCommand;
+import com.example.birdwikibot.telegram.command.subscription.SubscriptionMenu;
+import com.example.birdwikibot.telegram.command.subscription.UnsubscribeCommand;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+public class Menu extends HashMap<String, SuperCommand> {
+
+    public Menu(RandomCommand randomCommand,
+                SubscriptionMenu subscriptionMenu,
+                SubscribeCommand subscribeCommand,
+                UnsubscribeCommand unsubscribeCommand,
+                ChangeTimeCommand changeTimeCommand,
+                BackCommand backCommand) {
+
+        put(CommandNames.RANDOM_COMMAND.getId(), randomCommand);
+        put(CommandNames.RANDOM_COMMAND.getKey(), randomCommand);
+        put(CommandNames.SUBSCRIPTION_MENU.getId(), subscriptionMenu);
+        put(CommandNames.SUBSCRIPTION_MENU.getKey(), subscriptionMenu);
+        put(CommandNames.SUBSCRIBE_COMMAND.getKey(), subscribeCommand);
+        put(CommandNames.UNSUBSCRIBE_COMMAND.getKey(), unsubscribeCommand);
+        put(CommandNames.CHANGE_TIME_COMMAND.getKey(), changeTimeCommand);
+        put(CommandNames.BACK_COMMAND.getKey(), backCommand);
+    }
+}
