a049f2af177f09b2f42dce8e9adb07d142885b1d
bee-joo <bukin-danila@yandex.ru>
2022-09-19 20:50:03 +0300

    Add new command names

--- src/main/java/com/example/birdwikibot/telegram/command/CommandNames.java
+++ src/main/java/com/example/birdwikibot/telegram/command/CommandNames.java
@@ -7,6 +7,8 @@ public enum CommandNames {
     RANDOM_COMMAND("/random", "Случайная птица"),
     SUBSCRIPTION_MENU("/subscription", "Ежедневная птица"),
     SUBSCRIBE_COMMAND("/subscribe", "Подписаться"),
+    CHANGE_TIME_COMMAND("/changetime", "Изменить время"),
+    BACK_COMMAND("/back", "Назад"),
     UNSUBSCRIBE_COMMAND("/unsubscribe", "Отписаться");
 
     private final String id;
