2d6f5442f0144d474eb6dcb7e6e131fcefa44ada
bee-joo <bukin-danila@yandex.ru>
2022-09-19 20:50:49 +0300

    New routing using menu map

--- src/main/java/com/example/birdwikibot/telegram/handler/MessageHandler.java
+++ src/main/java/com/example/birdwikibot/telegram/handler/MessageHandler.java
@@ -11,36 +11,14 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 public class MessageHandler {
 
     private final ReplyKeyboard replyKeyboard;
-    private final RandomCommand randomCommand;
-    private final SubscriptionMenu subscriptionMenu;
-    private final SubscribeCommand subscribeCommand;
-    private final UnsubscribeCommand unsubscribeCommand;
+    private final Menu menu;
 
     public SendMessage router(String route, String chatId) {
-        String text;
 
-        if (route.equals("/start")) {
-            text = "Привет! Я просто присылаю ссылку на Wikipedia со случайной птицей";
-            return getSimpleMessage(chatId, text);
-
-        } else if (route.equals(CommandNames.RANDOM_COMMAND.getId()) || route.equals(CommandNames.RANDOM_COMMAND.getKey())) {
-            return randomCommand.handle(chatId);
-
-        } else if (route.equals(CommandNames.SUBSCRIPTION_MENU.getId()) || route.equals(CommandNames.SUBSCRIPTION_MENU.getKey())) {
-            return subscriptionMenu.handle(chatId);
-
-        } else if (route.equals(CommandNames.SUBSCRIBE_COMMAND.getId()) || route.equals(CommandNames.SUBSCRIBE_COMMAND.getKey())) {
-            return subscribeCommand.handle(chatId);
-
-        } else if (route.equals(CommandNames.UNSUBSCRIBE_COMMAND.getId()) || route.equals(CommandNames.UNSUBSCRIBE_COMMAND.getKey())) {
-            return unsubscribeCommand.handle(chatId);
-
-        } else if (route.equals("Назад")) {
-            text = "Привет ещё раз!";
-            return getSimpleMessage(chatId, text);
-
+        if (menu.containsKey(route)) {
+            return menu.get(route).handle(chatId);
         } else {
-            text = "Я не понял ваше желание";
+            String text = "Я не понял ваше желание";
             return getSimpleMessage(chatId, text);
         }
     }
